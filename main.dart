bac84b9b4967e415f54035fb67505537f3241777
Isacco Costa <isacco.costa@studenti.unipd.it>
2022-05-24 18:00:03 +0200

    hello world

--- main.dart
+++ main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:city_app/screens/profilepage.dart';
-import 'package:city_app/screens/loginpage.dart';
 
 void main() {
   runApp(const MyApp());
-} //main
+}//main
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: LoginPage.route,
-      routes: {
-        LoginPage.route : (context) => const LoginPage(),
-        ProfilePage.route : (context) => const ProfilePage(),
-      },
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello, Flutter!'),
+        ),
+      ),
     );
-  } //build
+  }//build
 }//MyApp
