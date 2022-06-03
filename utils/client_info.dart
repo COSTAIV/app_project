035908d4ecb92ba0d415523f6a7f23081c394d56
Martina Battaglia <martinabttgl@gmail.com>
2022-06-03 15:51:14 +0200

    auth profilePage m

---
+++ utils/client_info.dart
@@ -0,0 +1,15 @@
+class ClientInfo {
+
+  // Fitbit Client ID (replace XXX with your client ID)
+  static const String fitbitClientID = '238BFW';
+
+  // Fitbit Client Secret (replace XXX with your client secret)
+  static const String fitbitClientSecret = 'e909cf711ef6d99379a013217eab33bf';
+
+  /// Auth Uri (replace XXX with your Uri)
+  static const String fitbitRedirectUri = 'example://fitbit/auth';
+
+  /// Callback scheme (replace XXX with your callback scheme)
+  static const String fitbitCallbackScheme = 'example';
+
+}//Strings
