797f0145ec7a7f099f80ba20b4b09db6f132c905
Anna Fiorentin <annafiorentin@gmail.com>
2022-06-03 17:04:37 +0200

    hkgkifukftt

--- main.dart
+++ main.dart
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-//anna
-=======
->>>>>>> master
 
 import 'package:flutter/material.dart';
 import 'package:city_app/screens/profilepage.dart';
@@ -10,40 +6,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
-<<<<<<< HEAD
-} //main
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-=======
-}//main
->>>>>>> bac84b9b4967e415f54035fb67505537f3241777
->>>>>>> master
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello, Flutter!'),
-        ),
-      ),
-    );
-  } //build
-}//MyApp
-<<<<<<< HEAD
-=======
->>>>>>> bac84b9b4967e415f54035fb67505537f3241777
-=======
 } //main
+
+
+
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -58,5 +27,3 @@ class MyApp extends StatelessWidget {
           );
   } //build
 }//MyApp
->>>>>>> 163409462579e8cc57dfbf216959720ce066f865
->>>>>>> master
