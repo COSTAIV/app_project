c080e4e1ba2c850d5ebfdbec06deac839a32b358
Anna Fiorentin <annafiorentin@gmail.com>
2022-06-15 18:38:21 +0200

    gvggg

--- main.dart
+++ main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:city_app/screens/profilepage.dart';
 import 'package:city_app/screens/loginpage.dart';
@@ -9,10 +8,9 @@ import 'package:city_app/repository/databaseRepository.dart';
 import 'package:city_app/screens/homePage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-Future<void> main() async {
 
-  Future<void> main() async {
-  //This is a special method that use WidgetFlutterBinding to interact with the Flutter engine. 
+Future<void> main() async {
+  //This is a special method that use WidgetFlutterBinding to interact with the Flutter engine.
   //This is needed when you need to interact with the native core of the app.
   //Here, we need it since when need to initialize the DB before running the app.
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,32 +21,29 @@ Future<void> main() async {
   //This creates a new DatabaseRepository from the AppDatabase instance just initialized
   final databaseRepository = DatabaseRepository(database: database);
 
-  //Here, we run the app and we provide to the whole widget tree the instance of the DatabaseRepository. 
+  //Here, we run the app and we provide to the whole widget tree the instance of the DatabaseRepository.
   //That instance will be then shared through the platform and will be unique.
   runApp(ChangeNotifierProvider<DatabaseRepository>(
     create: (context) => databaseRepository,
     child: MyApp(),
   ));
 
-
-
   //runApp(const MyApp());
 
-} //main
-
-
+  //main
+}
 
 class MyApp extends StatelessWidget {
-   MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-          return MaterialApp(
-            initialRoute: LoginPage.route,
-            routes: {
-              LoginPage.route: (context) => LoginPage(),
-              ProfilePage.route: (context) => ProfilePage(),
-              HomePage.route: (context) => HomePage(),
-            },
-          );
+    return MaterialApp(
+      initialRoute: LoginPage.route,
+      routes: {
+        LoginPage.route: (context) => LoginPage(),
+        ProfilePage.route: (context) => ProfilePage(),
+        HomePage.route: (context) => HomePage(),
+      },
+    );
   } //build
-}//MyApp
+} //MyApp
